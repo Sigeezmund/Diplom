d172f115ddc56131ef5474539e9700b73dfa4db7
Peretyatko Oleg <o.peretyatko@qiwi.com>
2020-04-24 19:48:15 +0300

    regrouping all code from diplom_source
    Create Module with MatrixHandler. Regroup all graphics on 3 file

---
+++ TridiagonalMatrixHandler.py
@@ -0,0 +1,83 @@
+import numpy as np
+from sympy import *
+
+from numba import jit, njit
+
+D = 0.5  # Коэффициент миграции
+a = 2.0  # Коэффициент рождения новых людей
+K = 5.0  # количество доступных для жителей ресурсов
+deathKoeff = 1.  # Коэффициент смертность населения
+
+N = 100  # количество точек по оси OX (Площадь занимаемая людьми)
+x0 = 0.  # начало отрезка
+L = 100.  # конец отрезка
+h = (L - x0) / (N - 1)  # шаг по OX
+
+KT = 1000  # количество точек по времени
+t0 = 0.  # начальный момент времени
+T = 100.  # конечный момент времени
+tau = (T - t0) / (KT - 1)  # шаг по времени
+
+sigma = tau * D / h ** 2  # sigma - число Куранта
+
+A = np.zeros((N - 2, N - 2))
+d = np.zeros((N - 2))
+
+
+@njit
+def thomasAlgorithm(A, d):  # Tridiagonal matrix algorithm . Или метод прогонки
+    n = len(d)
+    P = np.zeros(n - 1)
+    Q = np.zeros(n - 1)
+    x = np.zeros(n)
+    P[0] = A[0][1] / -A[0][0]
+    Q[0] = -d[0] / -A[0][0]
+    for i in range(1, n - 1):  # находим прогоночные коэффициенты
+        a = A[i][i - 1]
+        b = A[i][i]
+        c = A[i][i + 1]
+
+        P[i] = c / (-b - a * P[i - 1])
+        Q[i] = (a * Q[i - 1] - d[i]) / (-b - a * P[i - 1])
+    x[-1] = (A[n - 1][n - 2] * Q[n - 2] - d[n - 1]) / (-A[n - 1][n - 1] - A[n - 1][n - 2] * P[n - 2])
+    for i in range(n - 2, -1, -1):  # Находим неизвекстные
+        x[i] = P[i] * x[i + 1] + Q[i]
+    return x
+
+
+@njit
+def solutionMatrixStart():  # заполнение матрицы решений краевыми условиями
+    x = np.linspace(x0, L, N)
+    u = np.zeros((N, KT))
+    for i in range(0, KT):
+        u[0][i] = 1
+        u[-1][i] = 0
+    for i in range(0, N):
+        u[i][0] = 0
+        u[i][-1] = 0
+    return u, x
+
+
+# @jit(nopython=True)
+def createAndSolveMatrix(kFunction):  # заполняем трехдиагональную матрицу Ax=d
+    u, x = solutionMatrixStart()
+    # print(u)
+    for i in range(1, KT):
+        A[0, 0] = - 2 * sigma - (a * tau * u[1][i - 1]) / kFunction(i, u[1][i - 1]) - deathKoeff * tau - 1
+        A[0, 1] = sigma
+        d[0] = (a * tau + 1) * (-u[1][i - 1]) - sigma * u[0][i]
+
+        for j in range(1, N - 3):
+            A[j, j - 1] = sigma
+            A[j, j] = - 2 * sigma - (a * tau * u[j + 1][i - 1]) / kFunction(i, u[j][i - 1]) - deathKoeff * tau - 1
+            A[j, j + 1] = sigma
+            d[j] = (a * tau + 1) * (-u[j + 1][i - 1])
+
+        A[N - 3, N - 3] = - 2 * sigma - (a * tau * u[N - 2][i - 1]) / kFunction(i,
+                                                                                u[N - 2][i - 1]) - deathKoeff * tau - 1
+        A[N - 3, N - 4] = sigma
+        d[N - 3] = (a * tau + 1) * (-u[N - 2][i - 1]) - sigma * u[N - 1][i]
+        # print (A,d)
+        u[1:N - 1, i] = thomasAlgorithm(A, d)
+        # print(u)
+    return u, x
